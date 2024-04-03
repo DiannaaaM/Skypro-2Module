49bd9c6c1d3bbaafa3a5173b2632b65af276053c
Student Free <murlaevadiana@gmail.com>
2024-04-03 08:18:34 +0300

    update code

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,15 @@
 from src.masks import mask_account, mask_card
+from scr.widget import new_mask_card, new_mask_account, get_data
 
 user_input = input()
 if len(user_input) == 20:
     print(mask_account(user_input))
 else:
     print(mask_card(user_input))
+
+user_input_card = input("номер карты: ")
+user_input_bill = input("номер счета: ")
+user_input_data = input('Дата(вида "2018-07-11T02:26:18.671407"): ')
+print(new_mask_card(user_input_card))
+print(new_mask_account(new_mask_account))
+print(get_data(user_input_data))
