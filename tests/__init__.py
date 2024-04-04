fe386050001fbd5b957962be9b1e42ab7a800dc5
Student Free <murlaevadiana@gmail.com>
2024-04-04 10:22:26 +0300

    update input and output

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,15 +1,13 @@
+from src.widget import get_data, mask_account_and_card
+
 from src.masks import mask_account, mask_card
-from scr.widget import new_mask_card, new_mask_account, get_data
 
-user_input = input()
-if len(user_input) == 20:
-    print(mask_account(user_input))
-else:
-    print(mask_card(user_input))
 
-user_input_card = input("номер карты: ")
-user_input_bill = input("номер счета: ")
+user_input = input("Номер карты:")
+print(mask_account(user_input))
+user_inp = input("Номер счета:")
+print(mask_card(user_inp))
+user_input_card_or_account = input('Информация типа карты/счета и номер карты/счета')
+print(mask_account_and_card(user_input_card_or_account))
 user_input_data = input('Дата(вида "2018-07-11T02:26:18.671407"): ')
-print(new_mask_card(user_input_card))
-print(new_mask_account(new_mask_account))
 print(get_data(user_input_data))
