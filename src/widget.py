1870ef62950d79f90a7c3d55f3cd79f3fb5454c0
Student Free <murlaevadiana@gmail.com>
2024-04-04 10:22:48 +0300

    update functions

--- src/widget.py
+++ src/widget.py
@@ -1,21 +1,4 @@
-def new_mask_card(information: str) -> str:
-    """Функция принимает на вход номер карты и возвращает ее маску"""
-    if "Visa" in information or "MasterCard" in information:
-        words = information.split()
-        masked_str = ""
-        for word in words:
-            if word.isdigit() and len(word) == 16:
-                masked_word = word[:2] + "** **** " + word[-4:]
-                masked_str += masked_word + " "
-            else:
-                masked_str += word + " "
-        return masked_str.strip()
-    else:
-        return information
-
-
-def new_mask_account(information: str) -> str:
-    """Функция принимает на вход номер счёта и возвращает его маску."""
+def mask_account_and_card(information: str) -> str:
     if "Счет" in information:
         words = information.split()
         masked_str = ""
@@ -26,10 +9,20 @@ def new_mask_account(information: str) -> str:
             else:
                 masked_str += word + " "
         return masked_str.strip()
+    if "Visa" in information or "MasterCard" in information:
+        words = information.split()
+        masked_str = ""
+        for word in words:
+            if word.isdigit() and len(word) >= 16:
+                masked_word = word[:4] + " " + word[6:8] + "** **** " + word[-4:]
+                masked_str += masked_word + " "
+            else:
+                masked_str += word + " "
+        return masked_str
     else:
         return information
 
 
 def get_data(d: str) -> str:
-    """ Функция, которая принимает на вход строку и возвращает строку с датой"""
-    return f'{d[8:10]}.{d[5:7]}.{d[0:4]}'
+    """Функция, которая принимает на вход строку и возвращает строку с датой"""
+    return f"{d[8:10]}.{d[5:7]}.{d[0:4]}"
