bf61556ece8becb9ebaf6d3c85f2c1e275eab230
DiannaaaM <murlaevadiana@gmail.com>
2024-04-13 17:53:31 +0300

    add new tests in "tests" directory for modules in "src"

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,12 @@
 def mask_card(number: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску"""
-    return number[:4] + " " + number[4:6] + "** **** " + number[-4:]
+    if len(number) == 16:
+        return number[:4] + " " + number[4:6] + "** **** " + number[-4:]
+    return number
 
 
 def mask_account(number: str) -> str:
     """Функция принимает на вход номер счёта и возвращает его маску."""
-    return "**" + number[-4:]
+    if len(number) == 20:
+        return "**" + number[-4:]
+    return number
