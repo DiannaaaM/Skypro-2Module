be38096ad8348ef0801ef927f71057a34e7baf0f
DiannaaaM <murlaevadiana@gmail.com>
2024-04-18 13:56:26 +0300

    fixing few mistakes

--- src/generators.py
+++ src/generators.py
@@ -1,8 +1,8 @@
 import random
-from typing import Generator, Iterator, List
+from typing import Any, Generator, Iterator, List
 
 
-def filter_by_currency(transactions: List[dict[str, int]], currency: str = "USD") -> Iterator[dict[str, int]]:
+def filter_by_currency(transactions: List[dict], currency: str = "USD") -> Iterator[dict]:
     """
     Функция, которая принимает список словарей,
     и возвращает итератор, который выдает по очереди операции, в которых указана заданная валюта.
@@ -12,20 +12,20 @@ def filter_by_currency(transactions: List[dict[str, int]], currency: str = "USD"
             yield transaction["id"]
 
 
-def returned_description(transactions: List[dict]) -> Generator[str, None, None]:
+def returned_description(transactions: List[dict]) -> Generator[Any, None, None]:
     """
     Генератор, который принимает список словарей и возвращает описание каждой операции по очереди.
     """
     for transaction in transactions:
-        yield str(transaction.get("description"))
+        yield transaction.get("description")
 
 
-def random_card_number(start: int, stop: int) -> Generator[int, None, None]:
+def random_card_number(start: int, stop: int) -> Generator[Any, None, None]:
     """
     Генератор номеров банковских карт, который должен генерировать номера карт
     в формате "XXXX XXXX XXXX XXXX", где X — цифра.
     Должны быть сгенерированы номера карт в заданном диапазоне
     """
     while True:
-        card_number = ''.join([str(random.randint(start, stop)) for _ in range(16)])
-        yield ' '.join(card_number[i:i+4] for i in range(0, len(card_number), 4))
+        card_number = "".join([str(random.randint(start, stop)) for _ in range(16)])
+        yield " ".join(card_number[i : i + 4] for i in range(0, len(card_number), 4))
