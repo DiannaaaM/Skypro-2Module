7f4d1174b4080e331dae87eec9c9c3e74943dcf5
DiannaaaM <murlaevadiana@gmail.com>
2024-04-21 08:40:45 +0300

    Fixing decorator

--- src/decorators.py
+++ src/decorators.py
@@ -1,22 +1,37 @@
 import datetime
+import logging
 from functools import wraps
 from typing import Any, Callable
 
-logfile = open("mylog.txt", "a", encoding="utf-8")
+
+def log(filename: str | None = None) -> Callable:
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                # Действие и запись в log_message сообщение, которое будет записывать в файл
+                logging.info("Время выполнения: %s УСПЕШНО", datetime.datetime.now())
+                log_message = f"Время выполнения: {datetime.datetime.now()} УСПЕШНО"
+                result = func(*args, **kwargs)
+            except Exception as e:
+                # Действие если ошибка и запись в log_message сообщение, которуешь будет распечатано
+                logging.error("Время выполнения: %s ОШИБКА: %s", datetime.datetime.now(), e)
+                log_message = f"Время выполнения: {datetime.datetime.now()} ОШИБКА: {e}"
+                result = func(*args, **kwargs)
+            if filename:
+                with open(filename, "a", encoding="utf-8") as file:
+                    file.write(log_message)
+                file.close()
+            else:
+                print(log_message)
+            return result
+
+        return wrapper
+
+    return decorator
 
 
-def log(func: Callable) -> Callable:
-    """Генератор, который записывает дату и операцию в файл"""
-
-    @wraps(func)
-    def inner(*args: Any, **kwargs: Any) -> Any:
-        logfile.write(f"Дата: {datetime.datetime.now()}  УСПЕШНО\n")
-        return func(*args, *kwargs)
-
-    return inner
-
-
-@log
+@log(filename="mylog.txt")
 def return_date_and_operation(date: str, operation: str) -> str:
     """Возвращает строку с датой и операцией"""
     return f"Время операции: {date}. Операция: {operation}"
