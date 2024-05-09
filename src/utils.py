81d1999b59509c6563ad9b351b66a2374bd46b49
DiannaaaM <murlaevadiana@gmail.com>
2024-05-09 08:29:45 +0300

    update dict for function and test

--- src/utils.py
+++ src/utils.py
@@ -54,10 +54,22 @@ def sum_amount(transaction: List[dict]) -> float:
     return total
 
 
-transactions = [
-    {
-        "operationAmount": {"amount": "87941.37", "currency": {"name": "руб.", "code": "RUB"}},
-    }
-]
-
-print(sum_amount(transactions))
+# transactions = [
+#     {
+#         "id": 441945886,
+#         "state": "EXECUTED",
+#         "date": "2019-08-26T10:50:58.294041",
+#         "operationAmount": {
+#             "amount": "31957.58",
+#             "currency": {
+#                 "name": "руб.",
+#                 "code": "RUB"
+#             }
+#         },
+#         "description": "Перевод организации",
+#         "from": "Maestro 1596837868705199",
+#         "to": "Счет 64686473678894779589"
+#     }
+# ]
+#
+# print(sum_amount(transactions))
