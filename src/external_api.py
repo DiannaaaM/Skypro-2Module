0e8700d18ad2700701cb9df2372ee3e7e8ecf649
DiannaaaM <murlaevadiana@gmail.com>
2024-05-12 08:22:06 +0300

    add "logging" library in "external_API" module

--- src/external_api.py
+++ src/external_api.py
@@ -6,6 +6,12 @@ from typing import Any, Dict
 import requests
 from dotenv import load_dotenv
 
+from src.logger import setup_logging_for_external_API
+
+logger = setup_logging_for_external_API()
+logger.info("Application from utils starts....")
+
+logger.info("Running 'get_currency_rate' function")
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
@@ -18,3 +24,7 @@ def get_currency_rate(currency: Any) -> Any:
     response_data = json.loads(response.text)
     rate = response_data["rates"]["RUB"]
     return rate
+
+
+logger.info("End 'get_currency_rate' function")
+logger.info("Application from utils finished")
--- src/logger.py
+++ src/logger.py
@@ -6,6 +6,8 @@ if os.path.isfile(os.path.join("../src/utils.log")):
     os.remove(os.path.join("../src/utils.log"))
 if os.path.isfile(os.path.join("../src/masks.log")):
     os.remove(os.path.join("../src/masks.log"))
+if os.path.isfile(os.path.join("../src/external_API.log")):
+    os.remove(os.path.join("../src/external_API.log"))
 
 
 def setup_logging_for_utils() -> Any:
@@ -21,6 +23,16 @@ def setup_logging_for_utils() -> Any:
 def setup_logging_for_masks() -> Any:
     logger = logging.getLogger(__name__)
     file_handler = logging.FileHandler("masks.log")
+    logger.addHandler(file_handler)
+    file_formatter = logging.Formatter("%(asctime)s %(module)s %(levelname)s: %(message)s")
+    file_handler.setFormatter(file_formatter)
+    logger.setLevel(logging.INFO)
+    return logger
+
+
+def setup_logging_for_external_API() -> Any:
+    logger = logging.getLogger(__name__)
+    file_handler = logging.FileHandler("external_API.log")
     logger.addHandler(file_handler)
     file_formatter = logging.Formatter("%(asctime)s %(module)s %(levelname)s: %(message)s")
     file_handler.setFormatter(file_formatter)
