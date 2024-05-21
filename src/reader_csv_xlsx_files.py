8d63007d5cd0f3a3e840fef23adf1ce40e92dc46
DiannaaaM <murlaevadiana@gmail.com>
2024-05-21 12:24:46 +0300

    add data base, little update function

--- src/reader_csv_xlsx_files.py
+++ src/reader_csv_xlsx_files.py
@@ -5,8 +5,16 @@ import pandas as pd
 
 
 def open_file(file_path: Any) -> str:
-    with open(file_path, "r", encoding="latin1") as f:
-        return f.read()
+    if file_path.endswith(".csv"):
+        with open(file_path, "r", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            return list(reader)
+    elif file_path.endswith(".xlsx"):
+        df = pd.read_excel(file_path)
+        return df.head()
+    else:
+        with open(file_path, "r", encoding="latin1") as f:
+            return f.read()
 
 
 # print(open_file('../data/transactions.csv'))  # 147761
