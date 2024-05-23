6470f444a26da18880ec36f7cfb91175d6cdd064
DiannaaaM <murlaevadiana@gmail.com>
2024-05-23 15:06:54 +0300

    little update function and test

--- src/reader_csv_xlsx_files.py
+++ src/reader_csv_xlsx_files.py
@@ -5,17 +5,32 @@ import pandas as pd
 
 
 def open_file(file_path: Any) -> Any:
+    transcriptions = []
     if file_path.endswith(".csv"):
         with open(file_path, "r", encoding="utf-8") as f:
-            reader = csv.DictReader(f)
-            return list(reader)
+            reader = csv.reader(f, delimiter=";")
+            for row in reader:
+                transcriptions.append(
+                    {
+                        "id": row[0],
+                        "state": row[1],
+                        "date": row[2],
+                        "amount": row[3],
+                        "currency_name": row[4],
+                        "currency_code": row[5],
+                        "from": row[6],
+                        "to": row[7],
+                        "description": row[8],
+                    }
+                )
+                # Добавляем строку в список
+        return transcriptions
     elif file_path.endswith(".xlsx"):
         df = pd.read_excel(file_path)
-        return df.to_dict()
+        return df.to_dict(orient="records")
     else:
-        with open(file_path, "r", encoding="latin1") as f:
-            return f.read()
+        return "Неверный формат файла"
 
 
-# print(open_file('../data/transactions.csv'))  # 147761
-# print(open_file('../data/transactions_excel.xlsx'))  # 81998
+# print(open_file('../data/transactions.csv'))
+print(open_file('../data/transactions_excel.xlsx'))
