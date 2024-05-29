dfaa1734a81d2c73f0ea060f7bbb2e8757cf992b
DiannaaaM <murlaevadiana@gmail.com>
2024-05-29 10:03:16 +0300

    fix some mistake, and 1 function and little update project

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import time
+from typing import Any
 
+from src.external_api import get_currency_rate
 from src.generators import filter_by_currency, random_card_number, returned_description
 from src.libraries import count_categories, filter_by_state
 from src.logger import setup_logging
 from src.masks import mask_account, mask_card
 from src.processing import sort_dicts_by_date, sorted_list_by_value
 from src.reader_csv_xlsx_files import open_file
-from src.utils import get_currency_rate, read_json_file, sum_amount
+from src.utils import read_json_file, sum_amount
 from src.widget import get_data, mask_account_and_card
 
 
-def calling_functions(user_input, path_to_file, type_of_sort, currency=None, sort_of_date=None, filter_by_word=None):
+def calling_functions(
+    user_input: str,
+    path_to_file: str,
+    type_of_sort: str,
+    currency: Any = None,
+    sort_of_date: Any = None,
+    filter_by_word: Any = None,
+) -> str:
+    """
+    Функция, которая обрабатывает данные о финансовых транзакциях из файла на основе пользовательского ввода.
+    """
     if user_input == "1":
         file = read_json_file(path_to_file)
     else:
@@ -21,7 +33,7 @@ def calling_functions(user_input, path_to_file, type_of_sort, currency=None, sor
         sort = sort_dicts_by_date(sort, reverse_value)
     if currency == "да":
         for value in sort:
-            value["amount"] = round(value["amount"] * get_currency_rate(), 2)
+            value["amount"] = round(value["amount"] * get_currency_rate(value["operationAmount"]["currency"]["code"]))
     if filter_by_word is not None:
         sort = filter_by_word(sort, filter_by_word)
     for value in sort:
@@ -30,7 +42,9 @@ def calling_functions(user_input, path_to_file, type_of_sort, currency=None, sor
         from_ = mask_account_and_card(value["from"])
         to = mask_account_and_card(value["to"])
         amount = value["operationAmount"]["amount"]
-        return f"""{date} {description} \n{from_} -> {to} \nСумма: {amount}"""
+    return f"""{date} {description}
+            {from_} -> {to} 
+            Сумма: {amount}"""
 
 
 print(
--- src/libraries.py
+++ src/libraries.py
@@ -1,6 +1,6 @@
 import re
 from collections import Counter
-from typing import Any
+from typing import Any, Dict
 
 
 def filter_by_state(transactions: list, search: str) -> list:
@@ -13,34 +13,41 @@ def filter_by_state(transactions: list, search: str) -> list:
     return result
 
 
-def count_categories(transactions: list, categories: Any):
+def count_categories(transactions: list) -> Dict[str, int]:
     """Функция, которая принимает на вход список словарей и возвращает словарь,
     в котором ключами являются категории операций, а значениями - количество операций в каждой категории."""
+    states_count: Dict[str, int] = Counter()
+    for transaction in transactions:
+        state = transaction.get("state")
+        if state is not None:
+            states_count.update([state])
+
+    return states_count
 
 
-transactions = [
-    {
-        "id": 441945886,
-        "state": "EXECUTED",
-        "date": "2019-08-26T10:50:58.294041",
-        "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
-        "description": "Перевод организации",
-        "from": "Maestro 1596837868705199",
-        "to": "Счет 64686473678894779589",
-    },
-    {
-        "id": 41428829,
-        "state": "EXECUTED",
-        "date": "2019-07-03T18:35:29.512364",
-        "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод организации",
-        "from": "MasterCard 7158300734726758",
-        "to": "Счет 35383033474447895560",
-    },
-]
-
-# result = count_categories(transactions, "EXECUTED")
+# transactions = [
+#     {
+#         "id": 441945886,
+#         "state": "CANCELED",
+#         "date": "2019-08-26T10:50:58.294041",
+#         "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+#         "description": "Перевод организации",
+#         "from": "Maestro 1596837868705199",
+#         "to": "Счет 64686473678894779589",
+#     },
+#     {
+#         "id": 41428829,
+#         "state": "EXECUTED",
+#         "date": "2019-07-03T18:35:29.512364",
+#         "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
+#         "description": "Перевод организации",
+#         "from": "MasterCard 7158300734726758",
+#         "to": "Счет 35383033474447895560",
+#     },
+# ]
+#
+# result = count_categories(transactions)
 # print(result)
 
-res = filter_by_state(transactions, "Перевод со ")
-print(res)
+# res = filter_by_state(transactions, "Перевод со ")
+# print(res)
